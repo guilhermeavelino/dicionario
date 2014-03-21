621cdfd3e146061fac127a55cc346371d1fc3449
Guilherme Avelino <guilherme.acecilio@gmail.com>
2014-03-21 11:54:55 -0300

    Melhorias no layout + barra de pesquisa

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Dicionario::Application.routes.draw do
   resources :terms
+  root 'terms#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
